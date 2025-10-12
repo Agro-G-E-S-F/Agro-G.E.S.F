c1baac5b4b82e59cc8733f45155fddabc6294b66
lipedeoliveira <felipe.vieira.7721@gmail.com>
2025-10-11 22:48:31 -0300

    chore: migra estrutura inicial do projeto Agro-G.E.S.F

--- src/inference/test_image.py
+++
@@ -1,4 +0,0 @@
-from test_model import PlantDiseasePredictor
-predictor = PlantDiseasePredictor('../../checkpoint.pth')
-result = predictor.predict_single_image('../../data/val/Saudavel/folha_soja.jpg')
-print('Resultado:', result)
